4f505a827e9663946cef72fccd41b355d60726f7
Shah Mohd Arhum <mohd.arhum28@gmail.com>
2025-03-13 15:24:51 +0530

    Small changes

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { motion } from "framer-motion";
 import { Image, Button, Snippet } from "@nextui-org/react";
 import { headingsDM, codestuff } from "@/config/fonts";
 import { Ghosts } from "@/components/ghosts";
+import {ArrowUpRight} from "react-feather";
 
 export default function Home() {
   const [isMobile, setIsMobile] = useState(false);
@@ -33,34 +34,8 @@ export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center h-[600px] sm:h-[900px]">
       <div className="max-w-7xl w-full text-center px-4 py-8 md:px-10 md:py-16 lg:px-20 lg:py-24">
-        {/* <DraggableSquare /> */}
-        <motion.h1
-          className={clsx(
-            "text-lg md:text-xl lg:text-2xl",
-            codestuff.className
-          )}
-        >
-          {"569: PAGE_UNDER_CONSTRUCTION"}
-        </motion.h1>
-        <br />
-        <motion.div
-          className="flex justify-center"
-          initial="hidden"
-          animate="visible"
-          variants={imageVariants}
-          transition={{ duration: 0.5, ease: "easeInOut" }}
-        >
-          <Image
-            src="https://media1.tenor.com/m/GOabrbLMl4AAAAAd/plink-cat-plink.gif"
-            className={clsx(
-              "text-center dark:border-1 dark:border-yellow-300 border-2 border-blue-950",
-              isMobile ? "w-36 h-36" : "w-60 h-60"
-            )}
-            alt="cat"
-            isBlurred
-          />
-        </motion.div>
-        <br></br>
+
+        <Ghosts />
         <motion.div
           id="text-wrapper"
           initial="hidden"
@@ -69,34 +44,20 @@ export default function Home() {
           variants={typewriterVariants}
           transition={{ delay: 0.5, duration: 0.5, ease: "easeInOut" }}
         >
-          {/* <Ghosts /> */}
-          <Snippet
-            hideCopyButton
-            variant="flat"
-            symbol=">>"
-            className={clsx(
-              "text-xs md:text-lg lg:text-lg font-black w-full sm:max-w-[800px] overflow-hidden text-left tracking-tighter break-words sm:mb-6 mb-4 dark:text-yellow-200 text-blue-950 px-5 py-2 "
-            )}
-          >
-            <span>{" hi, i'm Arhum"}</span>
-            <span>
-              {
-                "ive hired this cat to stare at you at all times so u better click the button below"
-              }
-            </span>
-            <span> {"oh and btw i dont like calling this a 'portfolio' "}</span>
-          </Snippet>
-          <br />
-          <Button
-            as={"a"}
-            variant="ghost"
-            size={isMobile ? "sm" : "lg"}
-            className="p-5"
-            href="./goodstuff"
-          >
-            {"exec ./goodstuff"}
-          </Button>
+          <p>WOW</p>
         </motion.div>
+        <Button
+            as={"a"}
+            variant="shadow"
+            href="./goodstuff"
+            endContent={<ArrowUpRight />}
+            className={clsx(
+                "mt-12 sm:p-10 p-8 rounded-2xl sm:text-2xl text-medium text-white bg-blue-950 dark:bg-gray-800 tracking-tighter",
+                headingsDM.className
+            )}
+        >
+          {"Next (get it?)"}
+        </Button>
       </div>
     </section>
   );
