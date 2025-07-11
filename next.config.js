2c8835331f6a70bfbf9747d915b651a5784b40a4
Shah Mohd Arhum <mohd.arhum28@gmail.com>
2025-07-11 23:21:39 +0530

    hopefully this does something

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 };
+const isExport = process.env.OUTPUT_EXPORT === '1';
 
 module.exports = {
   compiler: {
     styledComponents: true,
   },
+  ...(isExport ? { output: 'export' } : {})
 };
