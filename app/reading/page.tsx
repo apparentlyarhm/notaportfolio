b3913991552c2762f1bda5147cb51b8064aefb8a
Shah Mohd Arhum <mohd.arhum28@gmail.com>
2025-09-17 16:21:05 +0530

    cleanup

--- app/reading/page.tsx
+++
@@ -1,9 +0,0 @@
-import { title } from "@/components/primitives";
-
-export default function ReadingPage() {
-  return (
-    <div>
-      <h1 className={title()}>page is hidden now, will add content soon here!</h1>
-    </div>
-  );
-}
